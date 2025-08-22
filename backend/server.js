a18c904af705f9bc75b94e49781a37e7426d6160
moh-ammad <mohammadsohail9392@gmail.com>
2025-08-22 21:05:42 +0530

    new commit after deployment

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  'http://localhost:3000'
+  'http://localhost:3000',
+  process.env.CLIENT_URL
 ];
 
 const corsOptions = {
